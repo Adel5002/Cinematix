22620a89c68202f10c00e17b22fc99c4badb2192
Adel5002 <aromat29.11@gmail.com>
2023-06-06 16:19:51 +0300

    Docker adding

--- Cinematix_proj/settings.py
+++ Cinematix_proj/settings.py
@@ -174,8 +174,8 @@ STATICFILES_DIRS = [
 ]
 
 
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/'
+CELERY_BROKER_URL = 'redis://demo_app_redis:6379/'
+CELERY_RESULT_BACKEND = 'redis://demo_app_redis:6379/'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
